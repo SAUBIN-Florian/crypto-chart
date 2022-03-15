7233fd28b86a65c7af8ee129dfee63d4e18ee1eb
floriansaubin <saubinflorian@gmail.com>
2022-03-15 14:47:35 +0100

    workin' on home design

--- src/components/Informations.jsx
+++ src/components/Informations.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { ReactComponent as CryptoSvg } from '../assets/crypto.svg';
+import { ReactComponent as ArrowSvg } from '../assets/arrow.svg';
+import "../stylesheets/informations.css";
 
 export default function Informations() {
   return (
-    <div className="informations">Informations</div>
+    <div className="informations">
+      <div className="info-group-1">
+        <h1 className="info-group-1-title">Welcome on Cryptanalyze.</h1>
+        <p className="info-group-1-para">Here you will find multiple crypto-currencies chart in real time and usefull informations about the crypto's market.</p>
+      </div>
+      <div className="info-group-2">
+        <ArrowSvg className="info-arrow-svg" />
+        <h3 className="info-call-to-action">Select a <span className="call-to-action-span">tab</span> to start digging...</h3>
+      </div>
+      <CryptoSvg className="info-crypto-svg" />
+    </div>
   )
 }
